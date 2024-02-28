f564e9d36bef79af1b7ab773eb3d627ce93472a2
Puspa Chauhan <puspa@Puspas-MacBook-Air.local>
2024-02-28 11:11:42 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from'react-dom';
+// import { Component} from './Component';
+import Button from './Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App =()=>{
+    return<div>
+        <h1>UsefulWebsites</h1>
+        <h3> which website do you want o open?</h3>
+        <Button name='Google' link='http://google.com'/>
+        <Button name='Facebook' link='http://facebook.com'/>
+        <Button name='Twitter' link='http://twitter.com'/>
+        <Button name='Instagram' link='http://instagram.com'/>
+        <Button name='LinkedIn' link='http://linkedin.com'/>
+        <Button name='Pinterest' link='http://pinterest.com'/>
+        {/* <Button name='Tumblr' link='http://tumblr.com'/> */}
+        
+        
     </div>
-  );
 }
+
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Button =({name,link})=>{
+    return<a href={link} target = "_blank"><button className='button'>{name}</button></a>
+}
+export default Button;
